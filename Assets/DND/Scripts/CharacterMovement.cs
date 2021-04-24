7168f6f785e6f7df1777a0b8a12f67c014920894
Paul Straw <paulstraw@paulstraw.com>
2021-04-24 16:14:14 -0700

    Add basic move/jump controls and char prefabs

---
+++ Assets/DND/Scripts/CharacterMovement.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+  [SerializeField]
+  float jumpForce = 400f;
+
+  [Range(0, 0.3f)]
+  [SerializeField]
+  float movementSmoothing = 0.05f;
+
+  [SerializeField]
+  LayerMask groundLayers;
+
+  [SerializeField]
+  float moveSpeed;
+
+  [SerializeField]
+  private Transform groundCheck;
+
+  const float groundCheckRadius = 0.2f;
+
+  bool isGrounded = false;
+  Rigidbody2D rb;
+  bool facingRight = true;
+  Vector3 velocity = Vector3.zero;
+
+  void Awake()
+  {
+    rb = GetComponent<Rigidbody2D>();
+  }
+
+  void FixedUpdate()
+  {
+    isGrounded = false;
+
+    Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundCheckRadius, groundLayers);
+    for (int i = 0; i < colliders.Length; i++)
+    {
+      if (colliders[i].gameObject != gameObject)
+      {
+        isGrounded = true;
+      }
+    }
+  }
+
+  public void Move(float move, bool jump)
+  {
+    move *= moveSpeed;
+
+    Vector3 targetVelocity = new Vector2(move, rb.velocity.y);
+    rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, movementSmoothing);
+
+    if (move > 0 && !facingRight)
+    {
+      Flip();
+    }
+    else if (move < 0 && facingRight)
+    {
+      Flip();
+    }
+
+    if (isGrounded && jump)
+    {
+      isGrounded = false;
+      rb.AddForce(new Vector2(0, jumpForce));
+    }
+  }
+
+  void Flip()
+  {
+    facingRight = !facingRight;
+
+    Vector3 newScale = transform.localScale;
+    newScale.x *= -1;
+    transform.localScale = newScale;
+  }
+}
