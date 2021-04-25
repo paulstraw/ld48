08e9d1efd27fe9b367f3286e3821d64b73781049
Paul Straw <paulstraw@paulstraw.com>
2021-04-25 00:22:43 -0700

    Add basic reticle to Character prefab

---
+++ Assets/DND/Prefabs/Character/Reticle.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Reticle : MonoBehaviour
+{
+  [SerializeField]
+  float distanceMultiplier = 1;
+
+  CharacterMovement movement;
+
+  CharacterLifecycle lifecycle;
+
+  PlayerController playerController;
+
+  void Awake()
+  {
+    playerController = FindObjectOfType<PlayerController>();
+    movement = GetComponentInParent<CharacterMovement>();
+    lifecycle = GetComponentInParent<CharacterLifecycle>();
+  }
+
+  void Update()
+  {
+    bool moving = lifecycle.CharacterType == CharacterType.Dig ? playerController.diggerMoving : playerController.fighterMoving;
+
+
+    Vector2 move;
+    if (moving)
+    {
+      move = lifecycle.CharacterType == CharacterType.Dig
+        ? playerController.controls.Digger.Move.ReadValue<Vector2>()
+        : playerController.controls.Fighter.Move.ReadValue<Vector2>();
+
+      if (!movement.isFacingRight)
+      {
+        move.x = -move.x;
+      }
+    }
+    else
+    {
+      move = new Vector2(1, 0);
+    }
+
+    transform.localPosition = move * distanceMultiplier;
+  }
+}
